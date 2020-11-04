a18055ce500591053699853c4cfd9b54471c21fa
Jennifer Jung <jenjung2194@gmail.com>
2020-11-04 21:35:19 +0000

    edited wordsearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,47 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//Pair Programming with Jen & Romesh
 
-module.exports = wordSearch
+const transpose = (matrix) => {
+    const row = matrix.length;
+    let newArray = [];
+    const column = matrix[0].length;
+
+    for (let i = 0; i < column; i++) {
+        newArray.push([]);
+    }
+
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < column; j++) {
+            newArray[j].push(matrix[i][j]);
+        }
+    }
+    return newArray;
+};
+
+const wordSearch = (letters, word) => {
+    //check row
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    //check reverse
+    const reverseJoin = letters.map(ls => ls.reverse().join(''));
+    for (const l of reverseJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    //check column
+    const transposedArr = transpose(letters);
+    //flip the arrays
+    //and user horizontal join
+    const transposedJoin = transposedArr.map(arr => arr.join(''));
+    for (const l of transposedJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
+
+};
+
+module.exports = wordSearch;
